ac38ad15240fb68555f338ea14dcda230d01de92
Golovkov Anatoliy <20040007.zoxzox@gmail.com>
2026-05-08 17:40:47 +0300

    Update Triangle.cpp

--- Triangle.cpp
+++ Triangle.cpp
@@ -2,23 +2,19 @@
 #include <cmath>
 #include "Triangle.h"
 
-double Triangle::calculateSide(double x1, double y1, double x2, double y2) const {
-    return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+void Triangle::calculateSides() {
+    sideA = p1.distanceTo(p2);
+    sideB = p2.distanceTo(p3);
+    sideC = p3.distanceTo(p1);
 }
 
-Triangle::Triangle(double x1, double y1, double x2, double y2, double x3, double y3) {
-    this->x1 = x1; this->y1 = y1;
-    this->x2 = x2; this->y2 = y2;
-    this->x3 = x3; this->y3 = y3;
+Triangle::Triangle(const Point& point1, const Point& point2, const Point& point3)
+    : p1(point1), p2(point2), p3(point3) {
+    calculateSides();
     
-    sideA = calculateSide(x2, y2, x3, y3);
-    sideB = calculateSide(x1, y1, x3, y3);
-    sideC = calculateSide(x1, y1, x2, y2);
-    
-    // Проверка на вырожденный треугольник (точки на одной прямой)
     double area = getArea();
     if (area <= 0.000001) {
-        std::cout << "Error. Points are collinear, triangle does not exist." << std::endl;
+        std::cout << "Ошибка. Точки лежат на одной прямой, треугольник не существует." << std::endl;
         exit(1);
     }
 }
@@ -28,7 +24,6 @@ double Triangle::getPerimeter() const {
 }
 
 double Triangle::getArea() const {
-    // Формула Герона
     double p = getPerimeter() / 2;
     return sqrt(p * (p - sideA) * (p - sideB) * (p - sideC));
 }
