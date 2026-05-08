4fb36ee19bb912050f6f96d2496b50ca68d4940d
Golovkov Anatoliy <20040007.zoxzox@gmail.com>
2026-05-08 17:51:08 +0300

    Update Point.cpp

--- Point.cpp
+++ Point.cpp
@@ -3,13 +3,8 @@
 
 Point::Point(double x, double y) : x(x), y(y) {}
 
-double Point::getX() const {
-    return x;
-}
-
-double Point::getY() const {
-    return y;
-}
+double Point::getX() const { return x; }
+double Point::getY() const { return y; }
 
 double Point::distanceTo(const Point& other) const {
     return sqrt(pow(other.x - x, 2) + pow(other.y - y, 2));
