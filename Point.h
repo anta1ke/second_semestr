5c764032f60a2a381113a3eb6cd60c18b6894cb8
Golovkov Anatoliy <20040007.zoxzox@gmail.com>
2026-05-08 17:50:10 +0300

    Update Point.h

--- Point.h
+++ Point.h
@@ -1,37 +1,12 @@
 #pragma once
 
-/**
- * @brief Класс "Точка на плоскости"
- */
 class Point {
 private:
-    double x;
-    double y;
+    double x, y;
 
 public:
-    /**
-     * @brief Конструктор точки
-     * @param x координата X
-     * @param y координата Y
-     */
     Point(double x = 0, double y = 0);
-    
-    /**
-     * @brief Получить координату X
-     * @return координата X
-     */
     double getX() const;
-    
-    /**
-     * @brief Получить координату Y
-     * @return координата Y
-     */
     double getY() const;
-    
-    /**
-     * @brief Расстояние до другой точки
-     * @param other другая точка
-     * @return расстояние между точками
-     */
     double distanceTo(const Point& other) const;
 };
