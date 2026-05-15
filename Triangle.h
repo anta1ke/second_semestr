d3315d224424766d32e8664e3408ec76c5c10e7d
Golovkov Anatoliy <20040007.zoxzox@gmail.com>
2026-05-15 15:20:36 +0300

    Update Triangle.h

--- Triangle.h
+++ Triangle.h
@@ -1,16 +1,28 @@
 #pragma once
 #include "Point.h"
 
+/**
+ * @brief Класс треугольника, заданного тремя точками
+ */
 class Triangle {
 private:
-    Point p1, p2, p3;
-    double sideA, sideB, sideC;
+    Point p1, p2, p3;   ///< Три вершины треугольника
+    double sideA;        ///< Сторона BC (напротив p1)
+    double sideB;        ///< Сторона AC (напротив p2)
+    double sideC;        ///< Сторона AB (напротив p3)
 
-    void calculateSides() const;  // ← добавил const
+    /**
+     * @brief Вычисляет длину стороны между двумя точками
+     */
+    double calculateSide(const Point& a, const Point& b) const;
 
 public:
-    Triangle(const Point& point1, const Point& point2, const Point& point3);
-    
+    /**
+     * @brief Конструктор треугольника
+     * @param p1, p2, p3 - три вершины
+     */
+    Triangle(const Point& p1, const Point& p2, const Point& p3);
+
     double getPerimeter() const;
     double getArea() const;
     double getInscribedRadius() const;
