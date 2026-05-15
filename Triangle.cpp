cc674fae970692fab529cda369330e780288bbb8
Golovkov Anatoliy <20040007.zoxzox@gmail.com>
2026-05-15 15:21:06 +0300

    Update Triangle.cpp

--- Triangle.cpp
+++ Triangle.cpp
@@ -2,19 +2,19 @@
 #include <cmath>
 #include "Triangle.h"
 
-void Triangle::calculateSides() {
-    sideA = p1.distanceTo(p2);
-    sideB = p2.distanceTo(p3);
-    sideC = p3.distanceTo(p1);
+double Triangle::calculateSide(const Point& a, const Point& b) const {
+    return sqrt(pow(b.x - a.x, 2) + pow(b.y - a.y, 2));
 }
 
-Triangle::Triangle(const Point& point1, const Point& point2, const Point& point3)
-    : p1(point1), p2(point2), p3(point3) {
-    calculateSides();
-    
+Triangle::Triangle(const Point& p1, const Point& p2, const Point& p3)
+    : p1(p1), p2(p2), p3(p3) {
+    sideA = calculateSide(p2, p3);
+    sideB = calculateSide(p1, p3);
+    sideC = calculateSide(p1, p2);
+
     double area = getArea();
     if (area <= 0.000001) {
-        std::cout << "Ошибка. Точки лежат на одной прямой, треугольник не существует." << std::endl;
+        std::cout << "Ошибка. Точки коллинеарны, треугольник не существует." << std::endl;
         exit(1);
     }
 }
@@ -24,19 +24,14 @@ double Triangle::getPerimeter() const {
 }
 
 double Triangle::getArea() const {
-    double p = getPerimeter() / 2;
+    double p = (sideA + sideB + sideC) / 2;
     return sqrt(p * (p - sideA) * (p - sideB) * (p - sideC));
 }
 
 double Triangle::getInscribedRadius() const {
-    double S = getArea();
-    double P = getPerimeter();
-    if (P == 0) return 0;
-    return 2 * S / P;
+    return getArea() / (getPerimeter() / 2);
 }
 
 double Triangle::getCircumscribedRadius() const {
-    double S = getArea();
-    if (S == 0) return 0;
-    return (sideA * sideB * sideC) / (4 * S);
+    return (sideA * sideB * sideC) / (4 * getArea());
 }
